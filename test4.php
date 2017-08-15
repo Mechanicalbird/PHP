63444e9981ac7d58e728fed50351f0646af57558
Mechanicalbird <mechanicalbird_just@yahoo.com>
2017-08-15 17:19:32 +0900

    Add files via upload

---
+++ test4.php
@@ -0,0 +1,14 @@
+<?PHP
+
+$message = "Hello World";
+$message2 = "This is cool";
+$number = 1000;
+
+
+print "<h1>$message</h1>";
+print "<h3 align=center>$message2</h3>";
+print "The Number is: $number";
+print "I want this sentence \n </br> to show up on 2 lines";
+
+
+?>
